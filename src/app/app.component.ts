bd02751ac6ae1a39cb07912fd52e49b3b69a620d
unknown <emilio.mst@gmail.com>
2018-07-04 04:28:38 -0500

    added adjectives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Jellycore';
+  availableAdjectives: string[];
+  adjective: string;
+
+  constructor() {
+    this.setDefaults();
+    this.chooseAdjective();
+  }
+
+  setDefaults() {
+    this.availableAdjectives = [
+      "different"
+      , "original"
+      , "attractive"
+      , "alluring"
+      , "striking"
+      , "beautiful"
+      , "compelling"
+      , "impressive"
+      , "refreshing"
+      , "captivating"
+      , "provocative"
+      , "delightful"
+      , "engaging"
+    ];
+  }
+
+  chooseAdjective() {
+    var max = 0;
+
+    max = this.availableAdjectives.length;
+
+    if (max > 0)
+      this.adjective = this.availableAdjectives[getRandomInRange(0, max - 1)]; //include min, exclude max
+    else
+      this.adjective = DEFAULT_ADJECTIVE;
+  }
 }
+
+const getRandomInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const DEFAULT_ADJECTIVE: string = "cool";
